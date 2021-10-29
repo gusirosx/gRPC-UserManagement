2fad8215c67c9a9d8d380991f198eed598744346
Gustavo Rodrigues <gsrodrigues280@gmail.com>
2021-10-29 14:05:27 -0300

    Some comments

--- client/client.go
+++ client/client.go
@@ -11,26 +11,34 @@ import (
 )
 
 const (
+	//Address of the grpc server
 	address = "localhost:50051"
 )
 
 func main() {
+	//dial a connection to grpc server
 	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
+	//withBlock() means that this function will not return until the connection is made
 	defer conn.Close()
+
+	//create a new client (to pass the connection to that function)
 	c := pb.NewUserManagementClient(conn)
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 	var new_users = make(map[string]int32)
 	new_users["Alice"] = 43
 	new_users["Bob"] = 30
+	//call the create new user function by looping over the new users map
 	for name, age := range new_users {
 		r, err := c.CreateNewUser(ctx, &pb.NewUser{Name: name, Age: age})
+		//r - response from the grpc server
 		if err != nil {
 			log.Fatalf("could not create user: %v", err)
 		}
+		// show on terminal the User Details
 		log.Printf(`User Details:
 		NAME: %s
 		AGE: %d
--- server/server.go
+++ server/server.go
@@ -15,10 +15,12 @@ const (
 	port = ":50051"
 )
 
+//Implementation of the grpc service
 type UserManagementServer struct {
 	pb.UnimplementedUserManagementServer
 }
 
+//Defining the service method that is described in the proto file
 func (s *UserManagementServer) CreateNewUser(ctx context.Context, in *pb.NewUser) (*pb.User, error) {
 	log.Printf("Received: %v", in.GetName())
 	var user_id int32 = int32(rand.Intn(100))
@@ -26,13 +28,17 @@ func (s *UserManagementServer) CreateNewUser(ctx context.Context, in *pb.NewUser
 }
 
 func main() {
+	//Begin listening on the port specified
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
+	//Variable 's' is invoking the new server function from the grpc module
 	s := grpc.NewServer()
+	//After initialize this new server we're going to register the server as a new grpc service
 	pb.RegisterUserManagementServer(s, &UserManagementServer{})
 	log.Printf("server listening at %v", lis.Addr())
+	//Call the server
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
