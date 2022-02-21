afa3d9329d3e8b1deb4de989a0bb3fb0d6db37db
Gustavo Rodrigues <gsrodrigues280@gmail.com>
2022-02-21 16:27:49 -0300

    client improvements

--- client/client.go
+++ client/client.go
@@ -8,7 +8,11 @@ import (
 	pb "gRPC-usermngm/proto"
 
 	"github.com/gin-gonic/gin"
+	"google.golang.org/grpc"
 )
+
+// conn holds an open connection to the ping service.
+var conn *grpc.ClientConn
 
 func main() {
 	// Set up a http server.
@@ -18,36 +22,43 @@ func main() {
 		fmt.Fprintln(ctx.Writer, "Up and running...")
 	})
 
-	router.POST("/teste", func(ctx *gin.Context) {
-		// conn holds an open connection to the gRPC service
-		conn, err := Connection()
-		if err != nil {
-			log.Printf("failed to dial server %s: %v", *serverAddr, err)
-		}
-		defer conn.Close()
-		//create a new client (to pass the connection to that function)
-		client := pb.NewUserManagementClient(conn)
-		var user pb.NewUser
-		// Call BindJSON to bind the received JSON to user
-		err = ctx.BindJSON(&user)
-		if err != nil {
-			ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			log.Println(err.Error())
-			return
-		}
-		response, err := client.CreateNewUser(ctx, &user)
-		if err != nil {
-			ctx.JSON(http.StatusInternalServerError, gin.H{"error when creating the user": err.Error()})
-			log.Fatalln("Error when calling GetUser:", err.Error())
-			return
-		}
-		ctx.JSON(http.StatusOK, gin.H{"response": response})
-	})
+	router.POST("/teste", CreateNewUser)
 
 	// Run http server
 	if err := router.Run(":8081"); err != nil {
 		log.Fatalf("could not run server: %v", err)
 	}
+}
+
+// Function to create a new user
+func CreateNewUser(ctx *gin.Context) {
+	client := GetClient()
+	defer conn.Close()
+	var user pb.NewUser
+	// Call BindJSON to bind the received JSON to user
+	err := ctx.BindJSON(&user)
+	if err != nil {
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		log.Println(err.Error())
+		return
+	}
+	response, err := client.CreateNewUser(ctx, &user)
+	if err != nil {
+		ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error when creating the user": err.Error()})
+		log.Fatal("Error when calling GetUser:", err.Error())
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"response": response})
+}
+
+// Function to create a new client (to pass the connection to UserManagement function)
+func GetClient() pb.UserManagementClient {
+	var err error
+	conn, err = Connection()
+	if err != nil {
+		log.Printf("failed to dial server %s: %v", *serverAddr, err)
+	}
+	return pb.NewUserManagementClient(conn)
 }
 
 // // Initialize an empty message as input to the call to get users
