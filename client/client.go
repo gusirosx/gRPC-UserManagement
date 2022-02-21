71e3529330a07792d994e265c9ae33068c4301a3
Gustavo Rodrigues <gsrodrigues280@gmail.com>
2022-02-21 15:15:54 -0300

    REST-gRPC first steps using the context

--- client/client.go
+++ client/client.go
@@ -1,49 +1,61 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
-	"time"
+	"net/http"
 
 	pb "gRPC-usermngm/proto"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	// conn holds an open connection to the gRPC service
-	conn, err := Connection()
-	if err != nil {
-		log.Printf("failed to dial server %s: %v", *serverAddr, err)
-	}
-	defer conn.Close()
+	// Set up a http server.
+	router := gin.Default()
 
-	//create a new client (to pass the connection to that function)
-	client := pb.NewUserManagementClient(conn)
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-	var new_users = make(map[string]int32)
-	new_users["Alice"] = 43
-	new_users["Bob"] = 30
-	//call the create new user function by looping over the new users map
-	for name, age := range new_users {
-		r, err := client.CreateNewUser(ctx, &pb.NewUser{Name: name, Age: age})
-		//r - response from the grpc server
+	router.GET("/", func(ctx *gin.Context) {
+		fmt.Fprintln(ctx.Writer, "Up and running...")
+	})
+
+	router.POST("/teste", func(ctx *gin.Context) {
+		// conn holds an open connection to the gRPC service
+		conn, err := Connection()
 		if err != nil {
-			log.Fatalf("could not create user: %v", err)
+			log.Printf("failed to dial server %s: %v", *serverAddr, err)
 		}
-		// show on terminal the User Details
-		log.Printf(`User Details:
-		NAME: %s
-		AGE: %d
-		ID: %d`, r.GetName(), r.GetAge(), r.GetId())
+		defer conn.Close()
+		//create a new client (to pass the connection to that function)
+		client := pb.NewUserManagementClient(conn)
+		var user pb.NewUser
+		// Call BindJSON to bind the received JSON to user
+		err = ctx.BindJSON(&user)
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			log.Println(err.Error())
+			return
+		}
+		response, err := client.CreateNewUser(ctx, &user)
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error when creating the user": err.Error()})
+			log.Fatalln("Error when calling GetUser:", err.Error())
+			return
+		}
+		ctx.JSON(http.StatusOK, gin.H{"response": response})
+	})
+
+	// Run http server
+	if err := router.Run(":8081"); err != nil {
+		log.Fatalf("could not run server: %v", err)
 	}
-	// Initialize an empty message as input to the call to get users
-	params := &pb.GetUsersParams{}
-	r, err := client.GetUsers(ctx, params)
-	if err != nil {
-		log.Fatalf("could not retrieve users: %v", err)
-	}
-	log.Print("\nUSER LIST: \n")
-	// this call should retur an array of users that are stored within the user management servers
-	fmt.Printf("r.GetUsers(): %v\n", r.GetUsers())
 }
+
+// // Initialize an empty message as input to the call to get users
+// params := &pb.GetUsersParams{}
+// r, err := client.GetUsers(ctx, params)
+// if err != nil {
+// 	log.Fatalf("could not retrieve users: %v", err)
+// }
+// log.Print("\nUSER LIST: \n")
+// // this call should retur an array of users that are stored within the user management servers
+// fmt.Printf("r.GetUsers(): %v\n", r.GetUsers())
