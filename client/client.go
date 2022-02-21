3c407a3c307bddfb6b8ee02a1313244db63c3cda
Gustavo Rodrigues <gsrodrigues280@gmail.com>
2022-02-21 16:43:06 -0300

    client fully implemented in REST

--- client/client.go
+++ client/client.go
@@ -14,20 +14,14 @@ import (
 // conn holds an open connection to the ping service.
 var conn *grpc.ClientConn
 
-func main() {
-	// Set up a http server.
-	router := gin.Default()
-
-	router.GET("/", func(ctx *gin.Context) {
-		fmt.Fprintln(ctx.Writer, "Up and running...")
-	})
-
-	router.POST("/teste", CreateNewUser)
-
-	// Run http server
-	if err := router.Run(":8081"); err != nil {
-		log.Fatalf("could not run server: %v", err)
+// Function to create a new client (to pass the connection to UserManagement function)
+func GetClient() pb.UserManagementClient {
+	var err error
+	conn, err = Connection()
+	if err != nil {
+		log.Printf("failed to dial server %s: %v", *serverAddr, err)
 	}
+	return pb.NewUserManagementClient(conn)
 }
 
 // Function to create a new user
@@ -51,22 +45,33 @@ func CreateNewUser(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, gin.H{"response": response})
 }
 
-// Function to create a new client (to pass the connection to UserManagement function)
-func GetClient() pb.UserManagementClient {
-	var err error
-	conn, err = Connection()
+func GetUsers(ctx *gin.Context) {
+	client := GetClient()
+	defer conn.Close()
+
+	// Initialize an empty message as input to the call to get users
+	params := &pb.GetUsersParams{}
+	response, err := client.GetUsers(ctx, params)
 	if err != nil {
-		log.Printf("failed to dial server %s: %v", *serverAddr, err)
+		log.Fatalf("could not retrieve users: %v", err)
 	}
-	return pb.NewUserManagementClient(conn)
+	// this call should retur an array of users that are stored within the user management servers
+	ctx.JSON(http.StatusOK, gin.H{"response": response})
 }
 
-// // Initialize an empty message as input to the call to get users
-// params := &pb.GetUsersParams{}
-// r, err := client.GetUsers(ctx, params)
-// if err != nil {
-// 	log.Fatalf("could not retrieve users: %v", err)
-// }
-// log.Print("\nUSER LIST: \n")
-// // this call should retur an array of users that are stored within the user management servers
-// fmt.Printf("r.GetUsers(): %v\n", r.GetUsers())
+func main() {
+	// Set up a http server.
+	router := gin.Default()
+
+	router.GET("/", func(ctx *gin.Context) {
+		fmt.Fprintln(ctx.Writer, "Up and running...")
+	})
+
+	router.POST("/users", CreateNewUser)
+	router.GET("/users", GetUsers)
+
+	// Run http server
+	if err := router.Run(":8081"); err != nil {
+		log.Fatalf("could not run server: %v", err)
+	}
+}
