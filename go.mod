05ea1e3f3dc2d8ceb4009a38fccf3aa1ccdebdcf
Gustavo Rodrigues <gsrodrigues280@gmail.com>
2022-02-21 22:34:49 -0300

    change from pgx to lib/pq

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.17
 
 require (
 	github.com/gin-gonic/gin v1.7.7
-	github.com/jackc/pgx/v4 v4.15.0
+	github.com/lib/pq v1.10.2
 	google.golang.org/grpc v1.44.0
 	google.golang.org/protobuf v1.27.1
 )
@@ -15,14 +15,8 @@ require (
 	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/go-playground/validator/v10 v10.4.1 // indirect
 	github.com/golang/protobuf v1.5.0 // indirect
-	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
-	github.com/jackc/pgconn v1.11.0 // indirect
-	github.com/jackc/pgio v1.0.0 // indirect
-	github.com/jackc/pgpassfile v1.0.0 // indirect
-	github.com/jackc/pgproto3/v2 v2.2.0 // indirect
-	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
-	github.com/jackc/pgtype v1.10.0 // indirect
 	github.com/json-iterator/go v1.1.9 // indirect
+	github.com/kr/pretty v0.1.0 // indirect
 	github.com/leodido/go-urn v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.12 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
@@ -33,5 +27,6 @@ require (
 	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
 	golang.org/x/text v0.3.6 // indirect
 	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 	gopkg.in/yaml.v2 v2.2.8 // indirect
 )
--- server/server.go
+++ server/server.go
@@ -9,7 +9,9 @@ import (
 
 	pb "gRPC-usermngm/proto"
 
-	"github.com/jackc/pgx/v4"
+	"database/sql"
+
+	_ "github.com/lib/pq"
 	"google.golang.org/grpc"
 )
 
@@ -24,8 +26,7 @@ func NewUserManagementServer() *UserManagementServer {
 
 //Implementation of the grpc service
 type UserManagementServer struct {
-	conn                *pgx.Conn
-	first_user_creation bool
+	conn *sql.DB
 	pb.UnimplementedUserManagementServer
 }
 
@@ -47,35 +48,20 @@ func (server *UserManagementServer) Run() error {
 // When user is added, read full userlist from file into userlist struct, then append new user and write new userlist back to file
 func (s *UserManagementServer) CreateNewUser(ctx context.Context, in *pb.NewUser) (*pb.User, error) {
 	log.Printf("Received: %v", in.GetName())
-	createSql := ` create table if not exists users(id SERIAL PRIMARY KEY, name text, age int);`
-	_, err := s.conn.Exec(context.Background(), createSql)
+	user := &pb.User{Name: in.GetName(), Age: in.GetAge()}
+	comand, err := s.conn.Prepare("insert into users(name, age) values ($1,$2)")
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Table creation failed: %v\n", err)
-		os.Exit(1)
+		log.Println("Unable to insert data:", err.Error())
 	}
-	s.first_user_creation = false
+	comand.Exec(user.Name, user.Age)
 
-	log.Printf("Received: %v", in.GetName())
-
-	created_user := &pb.User{Name: in.GetName(), Age: in.GetAge()}
-	tx, err := s.conn.Begin(context.Background())
-	if err != nil {
-		log.Fatalf("conn.Begin failed: %v", err)
-	}
-
-	_, err = tx.Exec(context.Background(), "insert into users(name, age) values ($1,$2)",
-		created_user.Name, created_user.Age)
-	if err != nil {
-		log.Fatalf("tx.Exec failed: %v", err)
-	}
-	tx.Commit(context.Background())
-	return created_user, nil
+	return user, nil
 }
 
 // New Reciver function
 func (s *UserManagementServer) GetUsers(ctx context.Context, in *pb.GetUsersParams) (*pb.UserList, error) {
 	var users_list *pb.UserList = &pb.UserList{}
-	rows, err := s.conn.Query(context.Background(), "select * from users")
+	rows, err := s.conn.Query("select * from users")
 	if err != nil {
 		return nil, err
 	}
@@ -97,33 +83,16 @@ func main() {
 	user := os.Getenv("PG_USER")
 	dbName := os.Getenv("PG_DB_PG")
 	host := os.Getenv("PG_HOST")
-	connection := fmt.Sprintf("postgres://%s:%s@%s:5432/%s", user, password, host, dbName)
-	var user_mgmt_server *UserManagementServer = NewUserManagementServer()
-	conn, err := pgx.Connect(context.Background(), connection)
+	var server *UserManagementServer = NewUserManagementServer()
+
+	connection := fmt.Sprintf("user=%s dbname=%s password=%s host=%s sslmode=disable", user, dbName, password, host)
+	conn, err := sql.Open("postgres", connection)
 	if err != nil {
-		log.Fatalf("Unable to establish connection: %v", err)
+		log.Println("Unable to establish connection::", err.Error())
 	}
-	defer conn.Close(context.Background())
-	user_mgmt_server.conn = conn
-	user_mgmt_server.first_user_creation = true
-	if err := user_mgmt_server.Run(); err != nil {
+	defer conn.Close()
+	server.conn = conn
+	if err := server.Run(); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
 }
-
-// _ "github.com/lib/pq"
-// )
-
-// // Get the environment credentials and opens a connection with the database
-// func DbConect() *sql.DB {
-// 	password := os.Getenv("PG_PASS")
-// 	user := os.Getenv("PG_USER")
-// 	dbName := os.Getenv("PG_DB_STORE")
-// 	host := os.Getenv("PG_HOST")
-// 	connection := fmt.Sprintf("user=%s dbname=%s password=%s host=%s sslmode=disable", user, dbName, password, host)
-// 	db, err := sql.Open("postgres", connection)
-// 	if err != nil {
-// 		log.Println("Unable to connect:" + err.Error())
-// 	}
-// 	return db
-// }
